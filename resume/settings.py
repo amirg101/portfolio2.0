7c20af7585d49cdcb7c17124e14e108a8e2e8df4
amirg101 <amirgazi101@gmail.com>
2020-11-07 20:04:53 +0530

    first3

--- resume/settings.py
+++ resume/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,"static"),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.GzipManifestStaticFilesStorage'
 
 
 MEDIA_URL='/images/'
