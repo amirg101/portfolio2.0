acb778405f3fe03c2ef10459dcc1d8c3021a07c4
amirg101 <amirgazi101@gmail.com>
2020-11-07 20:20:42 +0530

    first3

--- resume/settings.py
+++ resume/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     
-    'whitenoise.runserver_nostatic',
+    # 'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
 
